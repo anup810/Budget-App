cc7371dbdcfcd38991c52bbdff71ad529c8034c8
Anup Saud <anupsaud10@gmail.com>
2024-08-25 17:42:29 -0400

    Initial Commit

---
+++ Budget App/Budget_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Budget_AppApp.swift
+//  Budget App
+//
+//  Created by Anup Saud on 2024-08-25.
+//
+
+import SwiftUI
+
+@main
+struct Budget_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
