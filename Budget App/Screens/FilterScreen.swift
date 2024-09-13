fc8889295e8d6bc8e3edab42dca7d64998056240
Anup Saud <anupsaud10@gmail.com>
2024-09-13 08:05:26 -0400

    Add FilterScreen to filter expenses by tags - Implement tag-based filtering for expenses using Core Data predicates - Display filtered expenses dynamically based on selected tags - Add TagsView for selecting tags in the filter screen

---
+++ Budget App/Screens/FilterScreen.swift
@@ -0,0 +1,18 @@
+//
+//  FilterScreen.swift
+//  Budget App
+//
+//  Created by Anup Saud on 2024-09-13.
+//
+
+import SwiftUI
+
+struct FilterScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FilterScreen()
+}
